d34385c828ac11afbff475e708aa09f7f6679aeb
Emilien Baudet <emilien.baudet@gmail.com>
2021-09-08 14:02:48 +0200

    add speed and download button

---
+++ js/content.js
@@ -0,0 +1,11 @@
+mediaplugin = $(".mediaplugin_videojs");
+
+for (var i = 0; i < mediaplugin.length; i++) {
+    url = $('audio > source', mediaplugin[i]).attr('src');
+    $(mediaplugin[i]).append(function(){
+        $(this).append("<div class='grid-child'><a href='" + url + "' class='btn btn-secondary'>descargar el audio</a></div>");
+    });
+    $('audio', mediaplugin[i]).attr('data-setup', '{ "playbackRates": [0.5, 1, 1.5, 2] }');
+}
+
+$('.content-alignment-container').removeClass('no-overflow');
